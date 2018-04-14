34a95f079cfbc6462e982683064326947c05f3bc
tesseradecades <nwe8978@rit.edu>
2018-04-14 13:56:14 -0400

    testing time w/ worker

--- includes/demo_workers.js
+++ includes/demo_workers.js
@@ -1,7 +1,10 @@
-var i=0;
+var i;
+
+var d = new Date();
 
 function timedCount() {
-    i=i+1;
+    //i=i+1;
+    i = d.toLocaleTimeString();
     postMessage(i);
     setTimeout("timedCount()", 500);
 }
